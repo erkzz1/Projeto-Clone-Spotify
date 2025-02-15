4d2fc5f8370034d4534e88cbb1ea0bcd53a09269
erkzz1 <erickmathias.contato@gmail.com>
2025-02-15 18:11:44 -0300

    :wrench: Adição ou atualização de arquivos de configuração.

--- Back-end/api/server.js
+++ Back-end/api/server.js
@@ -1,20 +1,30 @@
 import express, { json } from 'express'
 import cors from 'cors'
 import { db } from './connect.js'
+import path from 'path'
+
+const __dirname = path.resolve()
+
 
 const app = express()
 const PORT = 3000
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send("Só vamos trabalhar com '/artists' e '/songs'")
+app.get('/api/', (req, res) => {
+  res.send("Só vamos trabalhar com '/api/artists' e '/api/songs'")
 })
-app.get('/artists', async (req, res) => {
+app.get('/api/artists', async (req, res) => {
   res.send(await db.collection('artists').find({}).toArray())
 })
-app.get('/songs', async (req, res) => {
+app.get('/api/songs', async (req, res) => {
   res.send(await db.collection('songs').find({}).toArray())
+})
+
+app.use(express.static(path.join(__dirname, '../Front-end/dist')))
+
+app.get('*', async (req, res) => {
+  res.sendFile(path.join(__dirname,'../Front-end/dist/index.html'))
 })
 
 app.listen(PORT, () => {
