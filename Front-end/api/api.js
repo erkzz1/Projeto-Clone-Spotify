1d3aa96f424f954350b7af7668dc909cd69d203c
erkzz1 <erickmathias.contato@gmail.com>
2025-02-15 18:36:53 -0300

    :hammer: Ajuste no process env

--- Front-end/api/api.js
+++ Front-end/api/api.js
@@ -1,8 +1,9 @@
 import 'dotenv/config'
 import axios from 'axios'
 
-const { NODE_ENV } = process.env
-const URL = NODE_ENV === 'development' ? 'http://localhost:3000/api' : '/api'
+//const { NODE_ENV } = process.env
+//const URL = 'http://localhost:3000/api'
+const URL = 'https://projeto-clone-spotify.onrender.com/api'
 
 const resArtists = await axios.get(`${URL}/artists`)
 const resSongs = await axios.get(`${URL}/songs`)
